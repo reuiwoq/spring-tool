c9f0504a7461a8e5a9208e9e817cd456c13c6cfd
reuiwoq <146545909+reuiwoq@users.noreply.github.com>
2025-09-04 15:56:18 +0900

    Add files via upload

---
+++ board_jwt/entity/Board.java
@@ -0,0 +1,25 @@
+package com.abcdefg.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="article_no")
+	private Integer id;
+	
+	@Column(length=100)
+	private String title;
+	
+	@Column(length=2000)
+	private String content;
+}
